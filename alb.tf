cd5abf2b0eed4ef83269fa1a84dd808574e623fa
Adnan <adnanshaikh11501@gmail.com>
2024-01-25 01:58:55 +0530

    listner private alb alb.tf

--- alb.tf
+++ alb.tf
@@ -18,3 +18,22 @@ resource "aws_lb" "alb" {
 #   }
 }
 
+#Creates Listners for Private alb
+
+resource "aws_lb_listener" "private" {
+  count             = var.INTERNAL ? 1 : 0
+
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
